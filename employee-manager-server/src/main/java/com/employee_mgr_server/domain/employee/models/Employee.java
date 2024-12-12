37ff24a10d282ea43e2777f8a713a8e901b74db2
Anthony D. Mays <anthony@morganlatimer.com>
2024-12-12 15:43:43 -0800

    fix: adds proper constructors for hibernate support

--- employee-manager-server/src/main/java/com/employee_mgr_server/domain/employee/models/Employee.java
+++ employee-manager-server/src/main/java/com/employee_mgr_server/domain/employee/models/Employee.java
@@ -9,9 +9,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 public class Employee {
